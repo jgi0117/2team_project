f9650a54b7c42abc52d4563778a7de51d3dfb2d8
송지현 <tinasong200074@gmail.com>
2026-09-22 22:45:44 +0900

    Add model3 pipeline and evaluation outputs

---
+++ src/평가지표/평가지표.py
@@ -0,0 +1,20 @@
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+    average_precision_score
+)
+
+
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred))
+print("Recall:", recall_score(y_test, y_pred))
+print("F1:", f1_score(y_test, y_pred))
+
+print("PR-AUC:", average_precision_score(y_test, y_prob))
+
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
